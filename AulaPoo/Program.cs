136f172c4db8daaf6749fa58c6dc802c334f1b4c
Gabriel Oliveira <gabrieloliveira.gos04@gmail.com>
2022-08-02 15:14:28 -0300

    Adicionar arquivos de projeto.

---
+++ AulaPoo/Program.cs
@@ -0,0 +1,10 @@
+﻿using AulaPoo;
+
+// Instância de Objeto
+Usuario usuario1 = new Usuario();
+
+usuario1.Nome = "Paulo";
+usuario1.Email = "paulo@email.com";
+usuario1.Senha = "123456";
+
+Console.WriteLine(usuario1.Logar());
